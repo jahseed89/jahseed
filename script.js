db7faa8b837ead267f5b1bc1527d243d88c1ac3f
jahseed89 <61988780+jahseed89@users.noreply.github.com>
2020-03-25 19:32:13 +0100

    Javascript task1

---
+++ script.js
@@ -0,0 +1,11 @@
+let myName = "Jahseed";
+let courses = "HTML, Css, JavaScript";
+
+console.log("My Name is: " + myName);
+console.log("The courses am taking are: " +courses);
+
+for(var counter = 1; counter <= 200; counter++) {
+	if(counter % 2 !== 0) {
+		console.log(counter)
+	}
+}
